2e59e17d61694fe9a34ffeeca93343559522c616
justynaziemichod <268431@student.pwr.edu.pl>
2022-11-18 00:22:50 +0100

    init

--- com/example/sorting_visualizer/BarChart.java
+++ com/example/sorting_visualizer/BarChart.java
@@ -1,5 +1,10 @@
 package com.example.sorting_visualizer;
 
+import javafx.animation.ParallelTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.TranslateTransition;
+import javafx.util.Duration;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -30,6 +35,34 @@ public class BarChart extends ArrayList<Bar> {
         double tmp = values[i];
         values[i] = values[j];
         values[j] = tmp;
+    }
+
+    public ParallelTransition SwapBars (int indexOfBar1, int indexOfBar2) {
+        double speed = 100;
+        Bar bar1 = this.get(indexOfBar1);
+        Bar bar2 = this.get(indexOfBar2);
+        TranslateTransition transition1 = new TranslateTransition(Duration.millis(speed), bar1);
+        TranslateTransition transition2 = new TranslateTransition(Duration.millis(speed), bar2);
+        ParallelTransition parallelTransition = new ParallelTransition();
+        transition1.setByX(31);
+        transition2.setByX(-31);
+        parallelTransition.getChildren().addAll(transition1,transition2);
+        this.SwapIndex(indexOfBar1, indexOfBar2);
+        return parallelTransition;
+    }
+
+    public SequentialTransition BubbleSort () {
+        SequentialTransition sequentialTransition = new SequentialTransition();
+        for (int i = 0; i < numOfBars ; i++) {
+            for (int j = 0; j < numOfBars - i - 1; j++ ) {
+                if (this.getValueOf(j) > this.getValueOf(j+1)) {
+                    this.SwapValues(j, j+1);
+                    ParallelTransition swap = SwapBars(j, j+1);
+                    sequentialTransition.getChildren().add(swap);
+                }
+            }
+        }
+        return sequentialTransition;
     }
 
 }
---
+++ com/example/sorting_visualizer/Visualizer.java
@@ -0,0 +1,46 @@
+package com.example.sorting_visualizer;
+
+import javafx.animation.SequentialTransition;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Visualizer extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        BorderPane borderPane = new BorderPane();
+        HBox hBox = new HBox();
+        Button sortBtn = new Button("Bubble Sort");
+        Button genBtn = new Button("Generate");
+        HBox buttons = new HBox(sortBtn,genBtn);
+        hBox.setAlignment(Pos.BOTTOM_CENTER);
+        BarChart barChart = new BarChart(30);
+        hBox.getChildren().addAll(barChart);
+        borderPane.setCenter(hBox);
+        borderPane.setTop(buttons);
+        Scene scene = new Scene(borderPane, 1200, 750);
+
+        sortBtn.setOnMouseClicked( e -> {
+            sortBtn.setDisable(true);
+            SequentialTransition sort = barChart.BubbleSort();
+            sort.play();
+            sortBtn.setDisable(false);
+        });
+
+        stage.setTitle("Sorting Visualization");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
